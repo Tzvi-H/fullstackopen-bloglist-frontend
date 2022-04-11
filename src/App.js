3d1145064ddd45156e2a0ae9e64b027f0590200b
Tzvi-H <16guitar@gmail.com>
2022-04-11 12:23:50 -0500

    completed 5.2

--- src/App.js
+++ src/App.js
@@ -15,11 +15,23 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async event => {
     event.preventDefault()
     
     try {
       const user = await loginService.login({ username, password })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
 
       setUser(user)
       setUsername('')
@@ -27,6 +39,11 @@ const App = () => {
     } catch (exception) {
       console.log('wrong credentials')
     }
+  }
+
+  const handleLogout = event => {
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogappUser')
   }
 
   const loginForm = () => (
@@ -65,6 +82,11 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <p>
+        {user.name} logged in 
+        <button onClick={handleLogout}>log out</button>
+      </p>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
