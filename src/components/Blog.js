1e06a984a9af93df5c2888af2d92801ff826926b
Tzvi-H <16guitar@gmail.com>
2022-04-13 12:30:03 -0500

    completed 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [ showDetails, setShowDetails ] = useState(false)
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const detailView = () => (
+    <>
+    <br/>{blog.url}
+    <br/>likes {blog.likes}<button>like</button>
+    <br/>{blog.user.name}
+    </>
+  )
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} 
+      <button onClick={toggleShowDetails}>
+        {showDetails ? 'hide' : 'view'}
+      </button>
+
+      {showDetails ?  detailView() : null}
+    </div>  
+  )
+}
 
 export default Blog
